6b255d0b762f9031c1a8c9a736446641754835bb
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-04-11 02:13:43 +0000

    mise a jour

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,14 +4,10 @@ import { AuthController } from './controllers/auth.controller';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { OtpToken } from 'src/auth/entities/otp-token.entity';
-import { CounterOtp } from 'src/auth/entities/counter-otp.entity';
-import { Customer } from 'src/customer/entities/customer.entity';
 import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [DatabaseModule, JwtModule.register({}), TypeOrmModule.forFeature([OtpToken, CounterOtp, Customer])],
+  imports: [DatabaseModule, JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService, JwtService, JwtStrategy, JwtRefreshStrategy],
 })
