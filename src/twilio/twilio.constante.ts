4c5feb425e22db5a40da405f25c86e313773465a
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-05-08 22:39:15 +0000

    mise a jour

--- src/twilio/twilio.constante.ts
+++
@@ -1,3 +0,0 @@
-export const ACCOUNT_SID = 'ACf94dd4f04ccaf030dbddc609623d2031';
-export const AUTH_TOKEN = '8ef075deba6342e56491c1802caca946';
-export const TWILIO_PHONE_NUMBER = '+18383323406';
--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { Twilio } from 'twilio';
-import { ACCOUNT_SID, AUTH_TOKEN, TWILIO_PHONE_NUMBER } from './twilio.constante';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TwilioService {
     private readonly twilioClient: Twilio;
+    private readonly accountSid: string;
+    private readonly authToken: string;
+    private readonly twilioPhoneNumber: string;
 
-    constructor() {
-        this.twilioClient = new Twilio(ACCOUNT_SID, AUTH_TOKEN);
+    constructor(private readonly configService: ConfigService) {
+        this.accountSid = this.configService.get<string>('ACCOUNT_SID') ?? "";
+        this.authToken = this.configService.get<string>('AUTH_TOKEN') ?? "";
+        this.twilioPhoneNumber = this.configService.get<string>('TWILIO_PHONE_NUMBER') ?? "";
+        this.twilioClient = new Twilio(this.accountSid, this.authToken);
     }
 
     async sendOtp(phoneNumber: string, otp: string) {
         try {
             await this.twilioClient.messages.create({
                 body: `Votre code de confirmation est ${otp}`,
-                from: TWILIO_PHONE_NUMBER,
+                from: this.twilioPhoneNumber,
                 to: phoneNumber,
             });
         } catch (error) {
