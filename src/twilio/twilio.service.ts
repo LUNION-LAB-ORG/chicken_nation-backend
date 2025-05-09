c4a05d71936c9205964450a5deb6f2350caf2b8e
AndersonKouadioDev <andersonkouadio0118@gmail.com>
2025-05-09 00:15:07 +0000

    mise a jour

--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -8,24 +8,44 @@ export class TwilioService {
     private readonly accountSid: string;
     private readonly authToken: string;
     private readonly twilioPhoneNumber: string;
+    private readonly twilioWhatsappNumber: string;
 
     constructor(private readonly configService: ConfigService) {
         this.accountSid = this.configService.get<string>('ACCOUNT_SID') ?? "";
         this.authToken = this.configService.get<string>('AUTH_TOKEN') ?? "";
         this.twilioPhoneNumber = this.configService.get<string>('TWILIO_PHONE_NUMBER') ?? "";
+        this.twilioWhatsappNumber = this.configService.get<string>('TWILIO_WHATSAPP_NUMBER') ?? "";
+
+
         this.twilioClient = new Twilio(this.accountSid, this.authToken);
     }
 
     async sendOtp(phoneNumber: string, otp: string) {
         try {
-            await this.twilioClient.messages.create({
-                body: `Votre code de confirmation est ${otp}`,
-                from: this.twilioPhoneNumber,
-                to: phoneNumber,
-            });
-        } catch (error) {
+            await this.sendMessage(phoneNumber, `Votre code de confirmation est : ${otp}`, "sms");
+        } catch (error: any) {
             console.error('Error sending OTP:', error);
             throw error;
         }
+    }
+
+    async sendMessage(phoneNumber: string, message: string, type: "sms" | "whatsapp") {
+        try {
+            await this.twilioClient.messages.create({
+                body: message,
+                from: type === "sms" ? this.twilioPhoneNumber : `whatsapp:${this.twilioWhatsappNumber}`,
+                to: type === "sms" ? this.formatNumber(phoneNumber) : `whatsapp:${this.formatNumber(phoneNumber)}`,
+            });
+        } catch (error: any) {
+            console.error('Error sending message:', error);
+            throw error;
+        }
+    }
+
+    formatNumber(phoneNumber: string) {
+        let phone = phoneNumber.replace(/\D/g, '');
+        if (!phone) return '';
+        if (!phone.startsWith('+')) phone = `+${phone}`;
+        return phone;   
     }
 }
