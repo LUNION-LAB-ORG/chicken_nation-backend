f97ef691d76bc970df1abed79e23c50118e76ef8
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-05-29 23:35:17 +0000

    add base url

--- src/modules/statistics/constantes/statistics.constante.ts
+++ src/modules/statistics/constantes/statistics.constante.ts
@@ -3,19 +3,19 @@ import { join } from 'path';
 
 export const icons = {
     revenue: {
-        url: join(process.env.HOST ?? "", 'uploads', 'assets/statistics/icons/money-bag.svg'),
+        url: join(process.env.BASE_URL ?? "", 'uploads', 'assets/statistics/icons/money-bag.svg'),
         color: "#FFC107"
     },
     menusSold: {
-        url: join(process.env.HOST ?? "", 'uploads', 'assets/statistics/icons/food.svg'),
+        url: join(process.env.BASE_URL ?? "", 'uploads', 'assets/statistics/icons/food.svg'),
         color: "#FFC107"
     },
     totalOrders: {
-        url: join(process.env.HOST ?? "", 'uploads', 'assets/statistics/icons/shopping-cart.svg'),
+        url: join(process.env.BASE_URL ?? "", 'uploads', 'assets/statistics/icons/shopping-cart.svg'),
         color: "#FFC107"
     },
     totalCustomers: {
-        url: join(process.env.HOST ?? "", 'uploads', 'assets/statistics/icons/users.svg'),
+        url: join(process.env.BASE_URL ?? "", 'uploads', 'assets/statistics/icons/users.svg'),
         color: "#FFC107"
     },
 }
