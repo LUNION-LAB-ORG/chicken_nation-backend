89fdd84322cc859750052f7dd0fbf6cf7557d366
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-06-05 02:47:47 +0000

    mise à jour logique de connexion AppGateway

--- src/socket-io/gateways/app.gateway.ts
+++ src/socket-io/gateways/app.gateway.ts
@@ -31,7 +31,7 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
     async handleConnection(client: Socket) {
         try {
             // Récupérer le token depuis les headers
-            const token = (client.handshake.query.token as string)?.replace('Bearer ', '') || '';
+            const token = client.handshake.query.token as string || '';
 
             // Récupérer le type d'utilisateur depuis les query params
             const userType = client.handshake.query.type as "user" | "customer"
