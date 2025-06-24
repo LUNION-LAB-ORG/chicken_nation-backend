7c9a0b5400d9fb79425c0d8bdceabc7c6e3126b5
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-06-24 17:28:56 +0000

    mise a jour notification restau

--- src/modules/restaurant/restaurant.module.ts
+++ src/modules/restaurant/restaurant.module.ts
@@ -3,11 +3,14 @@ import { RestaurantService } from './services/restaurant.service';
 import { RestaurantController } from './controllers/restaurant.controller';
 import { MenuModule } from 'src/modules/menu/menu.module';
 import { RestaurantEvent } from './events/restaurant.event';
+import { RestaurantNotificationsTemplate } from './templates/restaurant-notifications.template';
+import { RestaurantEmailTemplates } from './templates/restaurant-email.template';
+import { RestaurantListenerService } from './listeners/restaurant-listener.service';
 
 @Module({
   imports: [MenuModule],
   controllers: [RestaurantController],
-  providers: [RestaurantService, RestaurantEvent],
+  providers: [RestaurantService, RestaurantListenerService, RestaurantEvent, RestaurantNotificationsTemplate, RestaurantEmailTemplates],
   exports: [RestaurantService]
 })
 export class RestaurantModule { }
