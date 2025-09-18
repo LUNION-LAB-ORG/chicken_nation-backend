0163e7660c8d243acb41e33043f0079df33bcc6a
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2025-09-18 17:57:54 +0000

    feat: ajouter une méthode d'émission pour les tickets dans AppGateway

--- src/modules/support/websockets/support-websocket.service.ts
+++ src/modules/support/websockets/support-websocket.service.ts
@@ -5,73 +5,78 @@ import { ResponseTicketMessageDto } from "../dtos/response-ticket-message.dto";
 
 @Injectable()
 export class SupportWebSocketService {
-    private readonly logger = new Logger(SupportWebSocketService.name);
-    constructor(private appGateway: AppGateway) { }
+  private readonly logger = new Logger(SupportWebSocketService.name);
+  constructor(private appGateway: AppGateway) { }
 
-    emitNewTicket(ticket: ResponseTicketDto) {
-        this.logger.log(`Emitting new ticket event ${ticket.id}`);
-        this.appGateway.emitToBackoffice('new:ticket', ticket);
+  emitNewTicket(ticket: ResponseTicketDto) {
+    this.logger.log(`Emitting new ticket event ${ticket.id}`);
+    this.appGateway.emitToBackoffice('new:ticket', ticket);
+    this.appGateway.emitToTicket(ticket.id, 'new:ticket', ticket);
 
-        if (ticket.assignee?.id) {
-            this.logger.log(`Emitting assigned ticket event ${ticket.id} to user ${ticket.assignee.id}`);
-            this.appGateway.emitToUser(ticket.assignee.id, 'user', 'assigned:ticket', ticket);
-        }
-
-        if (ticket.customer?.id) {
-            this.logger.log(`Emitting created ticket event ${ticket.id} to customer ${ticket.customer.id}`);
-            this.appGateway.emitToUser(ticket.customer.id, 'customer', 'created:ticket', ticket);
-        }
-
-        // notifier le restaurant dont la commande est liée au ticket
-        if (ticket.order?.restaurantId) {
-            this.logger.log(`Emitting new ticket event ${ticket.id} to restaurant ${ticket.order.restaurantId}`);
-            this.appGateway.emitToRestaurant(ticket.order.restaurantId, 'new:ticket', ticket);
-        }
+    if (ticket.assignee?.id) {
+      this.logger.log(`Emitting assigned ticket event ${ticket.id} to user ${ticket.assignee.id}`);
+      this.appGateway.emitToUser(ticket.assignee.id, 'user', 'assigned:ticket', ticket);
     }
 
-    emitUpdateTicket(ticket: ResponseTicketDto) {
-        this.logger.log(`Emitting update ticket event ${ticket.id}`);
-        this.appGateway.emitToBackoffice('update:ticket', ticket);
-
-        if (ticket.assignee?.id) {
-            this.logger.log(`Emitting assigned ticket event ${ticket.id} to user ${ticket.assignee.id}`);
-            this.appGateway.emitToUser(ticket.assignee.id, 'user', 'assigned:ticket', ticket);
-        }
-
-        if (ticket.customer?.id) {
-            this.logger.log(`Emitting updated ticket event ${ticket.id} to customer ${ticket.customer.id}`);
-            this.appGateway.emitToUser(ticket.customer.id, 'customer', 'update:ticket', ticket);
-        }
-
-        // notifier le restaurant dont la commande est liée au ticket
-        if (ticket.order?.restaurantId) {
-            this.logger.log(`Emitting update ticket event ${ticket.id} to restaurant ${ticket.order.restaurantId}`);
-            this.appGateway.emitToRestaurant(ticket.order.restaurantId, 'update:ticket', ticket);
-        }
+    if (ticket.customer?.id) {
+      this.logger.log(`Emitting created ticket event ${ticket.id} to customer ${ticket.customer.id}`);
+      this.appGateway.emitToUser(ticket.customer.id, 'customer', 'created:ticket', ticket);
     }
 
-    emitNewTicketMessage(ticketId: string, { message, restaurantId }: { message: ResponseTicketMessageDto, restaurantId: string }) {
-        this.logger.log(`Emitting new ticket message event for ticket ${ticketId}`);
-        this.appGateway.emitToBackoffice('new:ticket_message', { ticketId, message });
+    // notifier le restaurant dont la commande est liée au ticket
+    if (ticket.order?.restaurantId) {
+      this.logger.log(`Emitting new ticket event ${ticket.id} to restaurant ${ticket.order.restaurantId}`);
+      this.appGateway.emitToRestaurant(ticket.order.restaurantId, 'new:ticket', ticket);
+    }
+  }
 
-        if (message.authorUser) {
-            this.logger.log(`Emitting new ticket message event for ticket ${ticketId} to user ${message.authorUser.id}`);
-            this.appGateway.emitToUser(message.authorUser.id, 'user', 'new:ticket_message', { ticketId, message });
-        }
+  emitUpdateTicket(ticket: ResponseTicketDto) {
+    this.logger.log(`Emitting update ticket event ${ticket.id}`);
+    this.appGateway.emitToBackoffice('update:ticket', ticket);
+    this.appGateway.emitToTicket(ticket.id, 'update:ticket', ticket);
 
-        if (message.authorCustomer) {
-            this.logger.log(`Emitting new ticket message event for ticket ${ticketId} to customer ${message.authorCustomer.id}`);
-            this.appGateway.emitToUser(message.authorCustomer.id, 'customer', 'new:ticket_message', { ticketId, message });
-        }
-
-        if (restaurantId) {
-            this.logger.log(`Emitting new ticket message event for ticket ${ticketId} to restaurant ${restaurantId}`);
-            this.appGateway.emitToRestaurant(restaurantId, 'new:ticket_message', { ticketId, message });
-        }
+    if (ticket.assignee?.id) {
+      this.logger.log(`Emitting assigned ticket event ${ticket.id} to user ${ticket.assignee.id}`);
+      this.appGateway.emitToUser(ticket.assignee.id, 'user', 'assigned:ticket', ticket);
     }
 
-    emitMessagesRead(ticketId: string) {
-        this.logger.log(`Emitting read ticket messages event for ticket ${ticketId}`);
-        this.appGateway.emitToBackoffice('read:ticket_messages', { ticketId });
+    if (ticket.customer?.id) {
+      this.logger.log(`Emitting updated ticket event ${ticket.id} to customer ${ticket.customer.id}`);
+      this.appGateway.emitToUser(ticket.customer.id, 'customer', 'update:ticket', ticket);
     }
+
+    // notifier le restaurant dont la commande est liée au ticket
+    if (ticket.order?.restaurantId) {
+      this.logger.log(`Emitting update ticket event ${ticket.id} to restaurant ${ticket.order.restaurantId}`);
+      this.appGateway.emitToRestaurant(ticket.order.restaurantId, 'update:ticket', ticket);
+    }
+  }
+
+  emitNewTicketMessage(ticketId: string, { message, restaurantId }: { message: ResponseTicketMessageDto, restaurantId: string }) {
+    this.logger.log(`Emitting new ticket message event for ticket ${ticketId}`);
+    this.appGateway.emitToBackoffice('new:ticket_message', { ticketId, message });
+    this.appGateway.emitToTicket(ticketId, 'new:ticket_message', { ticketId, message });
+
+    if (message.authorUser) {
+      this.logger.log(`Emitting new ticket message event for ticket ${ticketId} to user ${message.authorUser.id}`);
+      this.appGateway.emitToUser(message.authorUser.id, 'user', 'new:ticket_message', { ticketId, message });
+
+    }
+
+    if (message.authorCustomer) {
+      this.logger.log(`Emitting new ticket message event for ticket ${ticketId} to customer ${message.authorCustomer.id}`);
+      this.appGateway.emitToUser(message.authorCustomer.id, 'customer', 'new:ticket_message', { ticketId, message });
+    }
+
+    if (restaurantId) {
+      this.logger.log(`Emitting new ticket message event for ticket ${ticketId} to restaurant ${restaurantId}`);
+      this.appGateway.emitToRestaurant(restaurantId, 'new:ticket_message', { ticketId, message });
+    }
+  }
+
+  emitMessagesRead(ticketId: string) {
+    this.logger.log(`Emitting read ticket messages event for ticket ${ticketId}`);
+    this.appGateway.emitToBackoffice('read:ticket_messages', { ticketId });
+    this.appGateway.emitToTicket(ticketId, 'read:ticket_messages', { ticketId });
+  }
 }
