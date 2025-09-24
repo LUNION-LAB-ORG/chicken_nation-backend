f698fa60b4679005a7243018c096fb74ac741597
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-09-24 02:17:15 +0000

    test endpoints webhook

--- src/turbo/turbo.module.ts
+++ src/turbo/turbo.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TurboController } from './controllers/turbo.controller';
 import { TurboService } from './services/turbo.service';
+import { TurboWebhookService } from './services/turbo-webhook.service';
 
 @Module({
   controllers: [TurboController],
-  providers: [TurboService],
-  exports: [TurboService]
+  providers: [TurboService, TurboWebhookService],
+  exports: [TurboService, TurboWebhookService]
 })
 export class TurboModule { }
