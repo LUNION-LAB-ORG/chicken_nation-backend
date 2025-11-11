6bfb953e0a88d4560035e36a38fc6d2a3a349b5d
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-11-11 13:43:48 +0000

    import module paiement in kkipay

--- src/kkiapay/kkiapay.module.ts
+++ src/kkiapay/kkiapay.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KkiapayController } from 'src/kkiapay/kkiapay.controller';
 import { KkiapayService } from 'src/kkiapay/kkiapay.service';
+import { PaiementsModule } from 'src/modules/paiements/paiements.module';
 
 @Module({
+  imports: [PaiementsModule],
   controllers: [KkiapayController],
   providers: [KkiapayService],
   exports: [KkiapayService]
