8715e05041b69746e362e2383ca06bd943a9db4d
Anderson Kouadio <andersonkouadio0118@gmail.com>
2026-01-28 13:39:35 +0000

    maj

--- src/modules/order/services/order.service.ts
+++ src/modules/order/services/order.service.ts
@@ -43,7 +43,7 @@ export class OrderService {
     const customerData = await this.orderHelper.resolveCustomerData({ ...createOrderDto, customer_id: customer_id ?? customerId });
     
     // Récupérer le restaurant le plus proche
-    console.log("createOrderDto", createOrderDto);
+    console.log("address", address);
     const restaurant = await this.orderHelper.getClosestRestaurant({ restaurant_id: restaurant_id, address });
 
     // Récupérer les plats et vérifier leur disponibilité
